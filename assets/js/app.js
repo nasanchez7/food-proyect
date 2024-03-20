12dbb6c223d4123918dd748ff19422969e893f70
nasanchez7 <nadirblanco02@gmail.com>
2024-03-20 16:00:45 -0300

    empezando en pagina 6

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ window.addEventListener('load', () => {
     const selectCountryContainer = document.getElementById('selectCountryContainer')
     const buttonOpenCountry = document.getElementById('buttonOpenCountry')
     const flagCountry = document.getElementById('flagCountry')
-    if(buttonOpenCountry && selectCountryContainer){
+    if(buttonOpenCountry && selectCountryContainer && flagCountry){
         buttonOpenCountry.addEventListener('click', () => {
                 selectCountryContainer.classList.remove('d-none')
                 selectCountryContainer.classList.add('d-flex', 'flex-column', 'gap-4')
@@ -19,22 +19,26 @@ window.addEventListener('load', () => {
         flagCountry.addEventListener('click', () => {
             selectCountryContainer.classList.remove('d-none')
             selectCountryContainer.classList.add('d-flex', 'flex-column', 'gap-4')
-    })
-    }
-    countrys.forEach(item => {
-        const div = document.createElement('div');
-        div.classList.add('d-flex', 'align-items-center', 'gap-2');
-        div.innerHTML = '<img src="' + item.img + '" class="object-fit-contain">';
-        selectCountryContainer.appendChild(div); 
-        div.addEventListener('click', () => {
-            if(changeCountry && selectCountryContainer){
+        })
+        countrys.forEach(item => {
+            const div = document.createElement('div');
+            div.classList.add('d-flex', 'align-items-center', 'gap-2');
+            div.innerHTML = '<img src="' + item.img + '" class="object-fit-contain">';
+            selectCountryContainer.appendChild(div); 
+            if(div){
                 div.addEventListener('click', () => {
-                        selectCountryContainer.classList.add('d-none')
-                        selectCountryContainer.classList.remove('d-flex', 'flex-column', 'gap-4')
-                        flagCountry.src = item.img
+                    if(changeCountry && selectCountryContainer){
+                        div.addEventListener('click', () => {
+                                selectCountryContainer.classList.add('d-none')
+                                selectCountryContainer.classList.remove('d-flex', 'flex-column', 'gap-4')
+                                flagCountry.src = item.img
+                        })
+                    }
                 })
             }
-        })
-    });
+            
+        });
+    }
+    
     
 })
